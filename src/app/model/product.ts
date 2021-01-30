7cd8c78ae60d4bcb4a45ec92717b7a67d9ee59f3
ildike0512 <hajos.ildiko007@gmail.com>
2021-01-30 11:19:57 +0100

    Classes

---
+++ src/app/model/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number = 0;
+    catId: number = 0;
+    name: string = '';
+    description: string = '';
+    image: string = '';
+    price: number = 0;
+    stock: number= 0;
+    featured: boolean = true;
+    active: boolean = true;
+}
---
+++ src/app/service/productservice.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductserviceService {
+
+  /* list: Product[] = [{
+
+  },
+
+  ]
+ */
+  constructor() { }
+}
