db6e8645323b47254202af3e80fbf32326c17cee
rhetesi <rhetesi@gmail.com>
2021-01-31 12:08:26 +0100

    Push, but don't use

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+import { ProductCard } from 'src/app/product-card/product-card.component';
+
+/**
+ * teszteléshez direkt import, amit maj ki kell venni a végleges kódból
+ */
+import products from 'src/assets/products.json';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  List: any = products; // Tömb kell!!!
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+/**
+ *
+ *Ez a komponens hasonlóan működik mint a termék lapozó, de ez korlátlan számú terméket képes megjeleníteni.
+ * - Működése: input tulajdonságként kapja meg a termékek tömbjét. Ezeket bejárva (*ngFor) minden terméket egy product-card komponenssel jelenít meg.
+ * - Szűrés: generáljatok egy filter pipe -ot és azt alkalmazzátok a szűrésre.
+ * > Legyen egy input mező a termékek felett (de ezen a komponensen belül).
+ * > Amikor módosítják a taralmát, akkor módosuljon egy változó, pld: `phrase`.
+ * > Ez a változó legyen átadva a filternek, ami ez alapján szűrje le a kapott termékek tömbjét. Ezt ott tudjátok alkalmazni, ahol az *ngFor bejárja a termékeket, pld:
+ * ```html
+ * <tr *ngFor="let product of products | filter:phrase">
+ * ```
+ * > Működő példa: `https://github.com/Training360/str-angular-002/tree/main/angular-components-pipes`
+ *
+ * Gabi ProductCard komponensét kell használni a megjelenítéshez
+ */
