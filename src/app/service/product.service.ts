003072258facb6ab58815ab9750aec334be4c251
rhetesi <rhetesi@gmail.com>
2021-02-02 00:08:58 +0100

    resolve conflict

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
 import products from 'src/assets/products.json';
-<<<<<<< HEAD
 
-=======
->>>>>>> dev
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-<<<<<<< HEAD
 
   List: any = products;
 
-=======
-  List: any = products;
->>>>>>> dev
   constructor() { }
 }
