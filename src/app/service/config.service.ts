0fd928556623a7e9b6ba0ff1f46fe85c7d2515ed
rhetesi <rhetesi@gmail.com>
2021-02-07 14:39:06 +0100

    Undo all chaneges in ConfigService, because the ProductService needs to be modified

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,29 +1,3 @@
-/* import { Injectable } from '@angular/core'; // EZ BIZTOS MŰKÖDIK
-
-export interface IMenuItem {
-  text: string;
-  link: string;
-  icon?: string;
-}
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ConfigService {
-
-  appName: string = 'Hazai turizmus';
-
-  menuItems: IMenuItem[] = [
-    { text: 'Főoldal', link: '/', icon: 'home' },
-    { text: 'Kirándulások', link: '/cat01' },
-    { text: 'Fürdőlátogatás', link: '/cat02' },
-    { text: 'Admin', link: '/admin' },
-  ];
-
-  constructor() { }
-} */
-
-
 import { Injectable } from '@angular/core';
 
 export interface IMenuItem {
