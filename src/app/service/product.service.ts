f6772e627ebe2601ab609c73ef0b09b456b75259
rhetesi <rhetesi@gmail.com>
2021-02-07 14:57:14 +0100

    HttpClientModule imported to app.module.ts

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -11,8 +11,9 @@ export class ProductService {
   constructor() { }
 } */
 
+// import: Product, HttpClient, Observable
 import { Injectable } from '@angular/core';
-import products from 'src/assets/products.json';
+//import products from 'src/assets/products.json'; - ehelyett { Product } import
 import { Product } from 'src/app/model/product';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -22,16 +23,25 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
+  // add apiUrl
   apiUrl: string = 'http://localhhost:3000/products';
 
-  List: any = products;
+  //List: any = products;
 
+  //
   constructor(
     private http: HttpClient,
   ) { }
 
+  //getAll - read All
   getAll(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
   }
+
+  //update
+
+
+
+  //remove
 
 }
