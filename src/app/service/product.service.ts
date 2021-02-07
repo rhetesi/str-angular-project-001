4f8d47e695f29cb1defe8173540fa5b1585c6000
rhetesi <rhetesi@gmail.com>
2021-02-07 14:48:59 +0100

    Maybe we have getAll methode

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/* import { Injectable } from '@angular/core'; // EZ BIZTOS MŰKÖDIK!!!
 import products from 'src/assets/products.json';
 
 @Injectable({
@@ -9,4 +9,29 @@ export class ProductService {
   List: any = products;
 
   constructor() { }
+} */
+
+import { Injectable } from '@angular/core';
+import products from 'src/assets/products.json';
+import { Product } from 'src/app/model/product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl: string = 'http://localhhost:3000/products';
+
+  List: any = products;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
 }
